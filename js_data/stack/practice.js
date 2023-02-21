eb40c993233188b3bf8e0339ca9e9430ee1bb6ea
啊布 <1807493813@qq.com>
2023-02-21 10:33:17 +0800

    isBracketBalanced

--- js_data/stack/practice.js
+++ js_data/stack/practice.js
@@ -29,3 +29,35 @@ function baseConverter(decNumber, base) {
   }
   return baseString;
 }
+
+/*平衡圆括号*/
+function isBracketBalanced(str) {
+	/*
+		@str:圆括号字符串，比如
+			"()()"  "(()()())"  平衡
+			"(()"  非平衡, 左括号较多
+			"(()))))" 非平衡，右括号较多
+	*/
+	const pattern = /^(\(|\))*$/;
+	if(!pattern.test(str)) {
+		throw new Error("Your parameter can only include () character");
+	}
+  debugger
+	const stack = new Stack();
+	for(let i=0; i<str.length; ++i) {
+		if(str[i] === "(") {
+			stack.push("(");
+		}else {
+			if(!stack.pop()){
+				return false;
+			}
+		}
+	}
+ 
+	return stack.isEmpty();
+}
+ 
+console.log(isBracketBalanced("()()")); //true
+// console.log(isBracketBalanced("()())")); // false
+// console.log(isBracketBalanced("((()")); // false
+// console.log(isBracketBalanced("fafa"));  // throw error
